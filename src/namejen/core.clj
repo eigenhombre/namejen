ee3e32312ab24c684ad94455b4d94ff3418666eb
John Jacobsen <john@mail.npxdesigns.com>
2012-03-26 22:29:44 -0500

    Cleanup

--- src/namejen/core.clj
+++ src/namejen/core.clj
@@ -5,6 +5,8 @@
   (:import (javax.script ScriptEngineManager ScriptEngine))
   (:gen-class))
 
+(expectations/disable-run-on-shutdown)
+
 (defn make-nextmap [chainlen parts]
   "Make a map of next values in a Markov chain, given a vector of
    input sequences with length==chainlen+1, where the first <chainlen>
@@ -39,23 +41,34 @@
             next-next (rand-nth (nextmap next-current))]
         (recur next-current next-all next-next)))))
  
-(defn prefix [] (rand-nth ["Dr." "Mr." "Ms." "Mrs." "M." "Miss" "Sr." "Herr" "Mme."]))
-(defn generation [] (rand-nth ["Sir" "Sr." "Jr." "Jr." "I" "II" "III" "III" "IV" "V"]))
+
+(defn prefix [] (rand-nth ["Dr." "Mr." "Ms." "Mrs." "M." "Miss" "Sr." "Herr" "Mme." "Sir"]))
+(defn generation [] (rand-nth ["Sr." "Jr." "Jr." "I" "II" "III" "III" "IV" "V"]))
 (def suffixes ["Esq." "Ph.D." "LCPT" "MD" "LMA" "LMT"])
-(defn suffix [] (rand-nth ["Esq." "Ph.D." "LCPT" "MD" "LMA" "LMT"]))
+(defn suffix [] (rand-nth ["Esq." "R.N." "Ph.D." "LCPT" "MD" "LMA" "LMT"]))
 (defn num-names [] (rand-nth [1 2 2 2 2 2 2 2 2 2 2 3 3 5]))
 (defn choose-whether [n] (= (rand-int n) 0))
 
-(defn memberp [x l] (if ((set l) x) true false))  ; FIXME: remember proper function
+
+(defn in? [x l] (if (nil? (some #{x} l)) false true))
+
+(expect (in? 0 [0 1 2]) true)
+(expect (in? 3 [0 1 2]) false)
+
 
 (defn take-n [n l]
   (loop [ret [], n n]
     (if (= n 0)
       ret
       (let [choice (rand-nth l)]
-        (if (memberp choice ret)
+        (if (in? choice ret)
           (recur ret n)
           (recur (conj ret choice) (dec n)))))))
+
+(expect (set (take-n 3 [:a :b :c])) #{:a :b :c})
+(expect (in? (first (take-n 1 [:a :b :c])) [:a :b :c]) true)
+(expect (in? (first (take-n 1 [:a :b :c])) [:d :e :f]) false)
+
 
 (defn name-maker [chainlen namefile]
   (let [allnames (map (comp #(str % \newline) lower-case)
@@ -67,35 +80,28 @@
       (let [names (join " "                                             ;; Core names
                        (repeatedly (num-names)
                                    #(generate-single-name nextmap)))
-            w-title (if (choose-whether 5)                              ;; Add title (Ms., Dr., ...)
-                      (str (prefix) " " names)
-                      names)
-            w-gen (if (choose-whether 5)                                ;; Add Jr., III, etc.
-                    (str w-title " " (generation))
-                    w-title)
+            with-title (if (choose-whether 5)                           ;; Add title (Ms., Dr., ...)
+                         (str (prefix) " " names)
+                         names)
+            with-gen (if (choose-whether 5)                             ;; Add Jr., III, etc.
+                    (str with-title " " (generation))
+                    with-title)
             suffixes (set (take-n (rand-int (rand-int 3)) suffixes))    ;; Add Ph.D., ...
             with-suffixes (if (empty? suffixes)
-                            w-gen
-                            (str w-gen ", " (join ", " suffixes)))]
+                            with-gen
+                            (str with-gen ", " (join ", " suffixes)))]
         with-suffixes))))
 
-
-(defn print-lines [l] (doseq [i l] (println i)))
 
 (defn get_resource [nm]
   "See http://stackoverflow.com/questions/2044394/how-to-load-program-resources-in-clojure"
   (ClassLoader/getSystemResource nm))
 
+
+(defn print-lines [l] (doseq [i l] (println i)))
+
+
 (defn -main []
   (print-lines
-   (repeatedly 500
+   (repeatedly 50
                (name-maker 4 (get_resource "names.txt")))))
-                           ; (.getFile (clojure.java.io/resource "names.txt"))))))
-                           ; was "/Users/jacobsen/Programming/Games/markovnamer/namefiles/all.txt"
-
-(expect (memberp 0 [0 1 2]) true)
-(expect (memberp 3 [0 1 2]) false)
-(expect (memberp (first (take-n 1 [:a :b :c])) [:a :b :c]) true)
-(expect (memberp (first (take-n 1 [:a :b :c])) [:d :e :f]) false)
-(expect (set (take-n 3 [:a :b :c])) #{:a :b :c})
-
