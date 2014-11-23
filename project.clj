c2fb60264bdaca502c12c07be1032519b1bbbe24
John Jacobsen <john@mail.npxdesigns.com>
2014-11-23 12:46:36 -0600

    Add license to project.clj.

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
   :url "https://github.com/eigenhombre/namejen"
   :resource-paths ["resources"]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main namejen.core
   :aliases {"autotest" ["midje" ":autotest"]}
   :description "A Markov-chain-based name generator for games, fiction, &c.")
