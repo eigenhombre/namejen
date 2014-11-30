c0b3814d948c49c9feda6f51bcd8cbaf782ed352
John Jacobsen <john@mail.npxdesigns.com>
2014-11-29 19:07:09 -0600

    Bump version for Clojars.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eigenhombre/namejen "0.1.2"
+(defproject eigenhombre/namejen "0.1.3"
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :url "https://github.com/eigenhombre/namejen"
   :resource-paths ["resources"]
