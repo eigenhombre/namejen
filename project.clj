c28cb8e58998586b9ca51512838992c74707c1eb
John Jacobsen <eigenhombre@gmail.com>
2021-11-05 11:50:07 -0500

    Version 0.1.16

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eigenhombre/namejen "0.1.16-SNAPSHOT"
+(defproject eigenhombre/namejen "0.1.16"
   :dependencies [[org.clojure/clojure "1.10.3"]]
   :url "https://github.com/eigenhombre/namejen"
   :resource-paths ["resources"]
