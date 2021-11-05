5ce4e0e8a90187b4b1393959f90be0785f2ad22e
John Jacobsen <eigenhombre@gmail.com>
2021-11-05 11:49:45 -0500

    Test/5 #5.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eigenhombre/namejen "0.1.15"
+(defproject eigenhombre/namejen "0.1.16-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.3"]]
   :url "https://github.com/eigenhombre/namejen"
   :resource-paths ["resources"]
